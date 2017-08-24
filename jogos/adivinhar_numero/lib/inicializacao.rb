91c96b8c14896ad2a61d7e43bfa80a359f4f9cb6
Antonino C.Gama <antoninocgama@hotmail.com>
2017-08-24 10:42:35 -0300

    comando clear adcionado

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,10 +1,13 @@
 class Inicializacao
 	def self.inicializando
+		system('clear')
 		print "Inicializando."
 		4.times do |i|
 		sleep 1
 		print "."
 	end
 		puts "."
+
+		system('clear')
 	end
 end
